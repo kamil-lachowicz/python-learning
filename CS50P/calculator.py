2f5a8df0177738f690924f31143106530c5444d0
Kamil Lachowicz <kamil.lachowicz18@gmail.com>
2026-05-25 20:26:21 +0200

    Initial commit of CS50P, back to reviewing absolute basics in Python.

---
+++ CS50P/calculator.py
@@ -0,0 +1,7 @@
+x = float(input("What's x?"))
+y = float(input("What's y?"))
+
+#We can also use round() instead of formating in print
+z = x/y
+
+print(f"{z:.2f}")
---
+++ CS50P/hello.py
@@ -0,0 +1,21 @@
+"""
+Lecture 0 of CS50P course. 
+Reviewing the absolute basics of programming in Python just to get into the course.
+"""
+
+# code . 
+# python hello.py
+
+# Ask user for their name
+name = input("What's your name? ") #We can also strip().title() here, at lines end
+
+#Remove whitespace from str and capitalize user's name
+name = name.strip().title()
+
+#Split user's name into first name and last name
+first, last = name.split(" ")
+
+
+# Say hello to the user in different formating
+print("Hello, \"respected\"", name, end="\n\n")
+print(f"hello, {last}")
